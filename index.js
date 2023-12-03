913bb44c2f7bcb62c3971b3e99a89d05432e4f26
orzcode <71000311+orzcode@users.noreply.github.com>
2023-12-03 17:10:52 +1300

    reverted

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ import cardManager from "./cardManager.js";
 //storage.clear;
 //Note: color icon css and card JS exists, but is 'display: none'd in cardManager
 
-// const firstLoad = (() => {
-//   storage.localArrays.notePool = storage.localStorage || [];
+const firstLoad = (() => {
+  storage.localArrays.notePool = storage.localStorage || [];
 
-//   storage.localArrays.trashPool = storage.trash || [];
+  storage.localArrays.trashPool = storage.trash || [];
 
-//   eventHandlers().formHandler();
-//   eventHandlers().dialogHandler();
-//   eventHandlers().popupHandler();
+  eventHandlers().formHandler();
+  eventHandlers().dialogHandler();
+  eventHandlers().popupHandler();
 
-//   actions().linksHandler();
-//   //appends navbar link actions
+  actions().linksHandler();
+  //appends navbar link actions
 
-//   cardManager().renderCards("formDiv");
-// })();
-
-document.querySelector("li").addEventListener("click", (event) => {
-  document.querySelector("#headerContainer").innerHTML = "Hahaha";
-})
+  cardManager().renderCards("formDiv");
+})();
