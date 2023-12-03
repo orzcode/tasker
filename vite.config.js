c9adddb2d24881954fdf50542847b3d464df79ea
orzcode <71000311+orzcode@users.noreply.github.com>
2023-12-03 17:41:00 +1300

    revert

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
 // vite.config.js
 export default {
-	resolve: {
-		alias: {
-		  '@': '/', // Adjust the alias to match your project structure
-		},
-		extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.vue'], // Include necessary file extensions
-	  },
+	// Your configuration options go here
   };
