f5b5b27ef276f6a7754c22aa50472b1fa7de1c0f
orzcode <71000311+orzcode@users.noreply.github.com>
2023-12-24 14:49:08 +1300

    checking tutorialCard

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { actions, eventHandlers } from "./dom.js";
 import storage from "./storage.js";
 import cardManager from "./cardManager.js";
 
-//storage.clear; - used to clear localstorage
+storage.clear;
+// - used to clear localstorage
 //Note: color icon css and card JS exists, but is 'display: none'd in cardManager
 
 const firstLoad = (() => {
